a7a40c28e11f7197db6a4bea3d2a241aece09510
syandhilakarno <if23.syandhilakarno@mhs.ubpkarawang.ac.id>
2024-11-28 16:53:23 +0700

    Delete Pertemuan-7 directory

--- Pertemuan-7/backend/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Menghapus semua session
-session_destroy(); // Menghancurkan session
-header('Location: ../index.php'); // Redirect ke halaman login
-exit();
-?>
